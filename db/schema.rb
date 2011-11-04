2a186c054b2b5714c9aefa11b9512a7315abe6ef
Evan Miller <evanmiller67@gmail.com>
2011-11-04 13:06:08 -0400

    change the name to be more appropriate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111102145354) do
+ActiveRecord::Schema.define(:version => 20111102152403) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
