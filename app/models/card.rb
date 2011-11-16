3597cdd9d8210d40c4fa2fa497a1d29cede6395e
Evan Miller <evanmiller67@gmail.com>
2011-11-16 17:28:19 -0500

    remove date from view and model object

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,15 +1,13 @@
 class Card
-  attr_accessor :name, :supervisor, :date, :projects, :allocations
+  attr_accessor :name, :supervisor, :projects, :allocations
   
   def initialize(args = {})
     args.reverse_merge!( { :name        => nil,
                            :supervisor  => nil,
-                           :date        => nil,
                            :projects    => [], 
                            :allocations => []})
     @name = args[:name]
     @supervisor = args[:supervisor]
-    @date = args[:date]
     @projects = args[:projects]
     @allocations = args[:allocations]
   end
